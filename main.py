9a58e5b7b90288ca1f82198e7b69514f43095322
Jimmy Burgess <jimmyburgess@jimmys-air.localdomain>
2018-04-24 15:42:06 +0200

    remove debug crap

--- main.py
+++ main.py
@@ -12,13 +12,6 @@ requests_toolbelt.adapters.appengine.monkeypatch()
 app = Flask(__name__)
 
 
-@app.errorhandler(500)
-def internal_error(error):
-
-    print "500!!!" + error.message
-    return jsonify({'whatev': True})
-
-
 @app.route('/recipes')
 @authorize(token_only=True)
 def get_recipe_pins(oauth_token):
