c24a7ce73086bfa340ee5ea883a3febc8ee6c630
Catherine Bostian <catherine.bostian@gmail.com>
2018-05-01 21:59:45 +0200

    add logic to transform recipe servings

--- services/pinterest.py
+++ services/pinterest.py
@@ -48,18 +48,48 @@ def transform_ingredients(pin):
     return pin
 
 
+def transform_servings(pin):
+    servings = pin['metadata']['recipe']['servings']
+
+    if not servings.get('serves') and not servings.get('yields'):
+        return pin
+
+    if servings.get('serves'):
+        if servings.get('serves').find('-') > -1:
+            average = sum(map(float, servings.get('serves').split('-')))/2
+            pin['metadata']['recipe']['servings'] = {'serves': average}
+        else:
+            pin['metadata']['recipe']['servings'] = {'serves': servings.get('serves', '')}
+
+        return pin
+
+    yields = int(''.join(filter(str.isdigit, servings.get('yields'))))
+    yield_units = ''.join(filter(lambda x: str.isalpha(x) or str.isspace(x),
+                                 servings.get('summary'))).replace('Makes', '').strip()
+
+    pin['metadata']['recipe']['servings'] = {'yields': yields, 'yield_units': yield_units}
+
+    return pin
+
+
+def transform(pin):
+    transform_servings(pin)
+    transform_ingredients(pin)
+    return pin
+
+
 def filter_recipes_only(pins):
     recipes = []
     for pin in pins:
         if pin.get('metadata', {}).get('recipe'):
-            recipes.append(transform_ingredients(pin))
+            recipes.append(transform(pin))
     return recipes
 
 
 def get_batch_of_recipes(oauth_token, cursor, query):
     if not os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
         # dont actually call pinterest in dev because they rate limit us
-        return {'data': [transform_ingredients(recipe) for recipe in deepcopy(RECIPE_SAMPLES)], 'cursor': ''}
+        return {'data': [transform(recipe) for recipe in deepcopy(RECIPE_SAMPLES)], 'cursor': ''}
 
     pins = []
     while len(pins) < 25:
